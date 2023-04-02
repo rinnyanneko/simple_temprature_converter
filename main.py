181f741d74030d0c159994e58a41302920df8fd6
凛にゃん <74105315+rinnyanneko@users.noreply.github.com>
2023-04-02 17:48:35 +0800

    Credits

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+#Credits: @rinnyanneko
+print("Simple Temperature Converter")
+print("Made by: @rinnyanneko")
+print("Version: 1.0")
+print("\n\n\n\n\n") # 5 new lines
+#mode selection
 print("Please enter mode: ")
 print("Celsius to Fahrenheit (c2f)")
 print("Fahrenheit to Celsius (f2c)")
 mode = input()
+#temperature conversion
 if mode == "c2f":
     print("Please enter temperature in Celsius: ")
     temp = input()
